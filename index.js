31f4433540f93bf5606d1a35dc6a146e6d5970b7
Iqra <iqra.ali@student.moringaschool.com>
2023-04-13 00:49:18 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const RightNumbers = dodger.style.Left.replace("px", "");
+    const Right = parseInt(RightNumbers, 10);
+  
+    if (Right > 0) {
+      dodger.style.Left = `${Left + 1}px`;
+    }
+  }
